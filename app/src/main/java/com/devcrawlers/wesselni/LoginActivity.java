b3454f827526ff92b41f269cac33608c09952ba0
ShadMaN <zakaria.hba.97@gmail.com>
2020-04-15 16:37:31 +0100

    Added login and register views

---
+++ app/src/main/java/com/devcrawlers/wesselni/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.devcrawlers.wesselni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    public void doLogin(View view){
+        Log.d("info", "Button login pressed!");
+    }
+
+    public  void goRegister(View view){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/devcrawlers/wesselni/RegisterActivity.java
@@ -0,0 +1,20 @@
+package com.devcrawlers.wesselni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void goLogin(View view){
+        this.finish();
+    }
+}
