4b9855671340f19bd85fc0185af9ad45eff602d0
aminjimzo <aminjimzo@gmail.com>
2020-04-30 18:59:47 +0000

    home fragment fixed

--- app/src/main/java/com/devcrawlers/wesselni/connection/Provider.java
+++ app/src/main/java/com/devcrawlers/wesselni/connection/Provider.java
@@ -1,13 +1,13 @@
 package com.devcrawlers.wesselni.connection;
 
 public interface Provider {
-    public String publicUrl="http://192.168.1.7:8000/uploads/files/";
-    public String url="http://192.168.1.7:8000/api/";
+    public String publicUrl="http://192.168.1.106:99/uploads/files/";
+    public String url="http://192.168.1.106:99/api/";
     public String offerSubUrl="offers";
     public String requestSubUrl="request";
     public String profileUrl="user";
     public String usersSubUrl="users/";
     public String citysSubUrl="cities";
     public String complaintsSubUrl="complaints";
-    
+    public String reservationsSubUrl="reservations";
 }
