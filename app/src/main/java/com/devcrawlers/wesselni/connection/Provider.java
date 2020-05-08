48f44b4de499e7246efd109004b9f3202bead562
nakigami <anas.devriani@gmail.com>
2020-05-08 22:54:49 +0000

    Amélioration du Design des interfaces suivantes :     - Ajout d'une réclamation     - Changement des icones du Drawer     - Ajout d'une nouvelle offre

--- app/src/main/java/com/devcrawlers/wesselni/connection/Provider.java
+++ app/src/main/java/com/devcrawlers/wesselni/connection/Provider.java
@@ -1,8 +1,8 @@
 package com.devcrawlers.wesselni.connection;
 
 public interface Provider {
-    public String publicUrl="http://192.168.1.6:99/uploads/files/";
-    public String url="http://192.168.1.6:99/api/";
+    public String publicUrl="http://192.168.1.20:99/uploads/files/";
+    public String url="http://192.168.1.20:99/api/";
     public String offerSubUrl="offers";
     public String requestSubUrl="request";
     public String profileUrl="user";
